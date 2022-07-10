4527af30c5cba9c3e82730fc7d33ff66a36f7b3c
hrk2023 <hrkdasgupta025@gmail.com>
2022-07-10 05:50:34 +0530

    feat: add Proocfile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from os import path
 
-from dotenv import load_dotenv
 from fastapi import FastAPI
 
 from patkai.database import engine
@@ -8,9 +7,6 @@ from patkai.models import admin_model as model
 from patkai.routers import admin_crud, boarders
 
 app = FastAPI()
-
-BASE_PATH = path.abspath(path.dirname(path.dirname(__file__)))
-load_dotenv(path.join(BASE_PATH,".env"))
 
 # Create all tables
 model.Base.metadata.create_all(bind=engine)
